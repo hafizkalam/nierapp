0b27eba42d87335eee4bad75ddb5317259e81a44
hafizkalam <hafizkalamabdillah@gmail.com>
2022-06-15 11:40:19 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { fromJSON } from 'postcss';
 import React from 'react';
-import { Header } from './components';
+import { Routes, Route } from "react-router-dom";
+import { CreateContainer, Header, MainContainer } from './components';
 
 const App = () => {
   return (
-    <div className='w-screen h-auto flex flex-col'>
+    <div className='w-screen h-auto flex flex-col bg-primary' >
       <Header />
+
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path="/*" element={<MainContainer />} />
+          <Route path="/createItem" element={<CreateContainer />} />
+        </Routes>
+      </main>
     </div>
   )
 }
